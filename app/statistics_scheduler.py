72c2b85c57c716dfa3f1b80d83759dc1a0eeb4ed
Andrii Smidonov <andrii@earthena.ai>
2025-10-06 21:54:34 +0100

    Update stats scheduler

--- app/statistics_scheduler.py
+++ app/statistics_scheduler.py
@@ -126,7 +126,7 @@ class StatisticsScheduler:
         # Відправка тижневої статистики: кожного понеділка о 09:00
         self.scheduler.add_job(
             self.send_weekly_statistics_to_users,
-            trigger=CronTrigger(day_of_week=0,hour=23, minute=20),  # Понеділок = 0
+            trigger=CronTrigger(day_of_week=0,hour=23, minute=58),  # Понеділок = 0
             id='send_weekly_statistics',
             name='Відправка тижневої статистики користувачам',
             replace_existing=True
